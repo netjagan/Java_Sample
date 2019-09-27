b4e5b6eaf952af136170691aecd4cf491978da9b
netjagan <netjagan@gmail.com>
2019-09-27 16:55:47 +0800

    First

--- src/main/java/Java_Sample/App.java
+++ src/main/java/Java_Sample/App.java
@@ -5,7 +5,7 @@ package Java_Sample;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world.... whats app!";
     }
 
     public static void main(String[] args) {
